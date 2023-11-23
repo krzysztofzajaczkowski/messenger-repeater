092872aa3208c7833d58b68dbe4e79ace1349bd4
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2023-11-23 22:12:36 +0100

    #4 Mess refactor (#5)
    * Refactor Mess class
    
    * Install TypeScript node types

--- src/mess.ts
+++ src/mess.ts
@@ -3,66 +3,99 @@ import fs from "fs";
 //@ts-ignore
 import login from "facebook-chat-api";
 import { config } from "./config";
-import { Mutex } from "async-mutex";
 import Bot from "./bot";
-
-console.log(`my user id: ${config.USER_ID}`);
-console.log(`thread id: ${config.THREAD_ID}`);
-
-const options = {
-  listenEvents: true,
-  selfListen: true,
-};
-
-const credentials = {
-  appState: JSON.parse(fs.readFileSync(config.COOKIES_FILE_PATH, 'utf8')),
-};
-
-let fbCookiesStored = false;
-process.on('SIGINT', () => process.exit());
-process.on('SIGTERM', () => process.exit());
 
 class Mess {
     api: any;
     bot?: Bot;
     lastUsername?: string;
+    fbCookiesStored: boolean = false;
+    supportedMessageTypesForProxy: string[] = [
+        'message',
+        'message_reply'
+    ];
+
+    private getOptions() {
+        return {
+            listenEvents: true,
+            selfListen: true,
+          };
+    }
+
+    private getCredentials() {
+        return {
+            appState: JSON.parse(fs.readFileSync(config.COOKIES_FILE_PATH, 'utf8')),
+          };
+    }
+
     constructor() {
-        
         // @ts-ignore
-        login(credentials, options, (err, api) => {
-            this.api = api;
-            // @ts-ignore
-            this.api.listenMqtt((err, message) => {
-                if (err) {
-                console.log('Error is in API LISTEN');
-                console.log(err);
-                } else {
-                if (!fbCookiesStored) {
-                    fs.writeFileSync(config.COOKIES_FILE_PATH, JSON.stringify(this.api.getAppState()));
-                    fbCookiesStored = true;
-                }
+        login(this.getCredentials(), 
+            this.getOptions(), 
+            this.matchCallback(api => {
+                this.api = api;
 
-                const isNotSentByMe = message.senderID !== config.USER_ID;
-                const isSentInCorrectConversation = message.threadID === config.THREAD_ID;
-                const isMessageOrReply = message.type === 'message' || message.type === 'message_reply';
+                this.onListen(this.matchCallback(message => {
+                    this.storeCookiesIfRequired();
 
-                if(isNotSentByMe && isSentInCorrectConversation && isMessageOrReply && this.bot) {
-                    // @ts-ignore
-                    this.api.getThreadInfo(message.threadID, async (err, threadInfo) => { 
-                        const senderName = threadInfo.nicknames[message.senderID];
-                        // @ts-ignore
-                        this.api.getUserInfo(message.senderID, (err, userInfo) => {
-                            const images = message.attachments.filter((a : any) => a.type === "photo").map((a : any) => a.largePreviewUrl);
-                            this.bot?.sendMeskaSrodaMessage(senderName, message.body, images, userInfo[message.senderID].thumbSrc);
-                        });
-                        
+                    const isNotSentByMe = message.senderID !== config.USER_ID;
+                    const isSentInCorrectConversation = message.threadID === config.THREAD_ID;
+                    const isMessageOrReply = this.supportedMessageTypesForProxy.includes(message.type);
+
+                    if(isNotSentByMe && isSentInCorrectConversation && isMessageOrReply && this.bot) {
+                        this.sendDiscordBotMessage(message);              
+                    }
+                }));
+            })
+        );
+    }
+
+    private matchCallback(onSuccess: (payload: any) => void) {
+        return (err: any, payload: any) => {
+            if(err) {
+                this.logError(err)
+                return;
+            }
+
+            onSuccess(payload);
+        };
+    }
+
+    private onListen(callback: (err: any, messge: any) => void) {
+        // @ts-ignore
+        this.api.listenMqtt(callback);
+    }
+
+    private storeCookiesIfRequired() {
+        if (!this.fbCookiesStored) {
+            fs.writeFileSync(config.COOKIES_FILE_PATH, JSON.stringify(this.api.getAppState()));
+            this.fbCookiesStored = true;
+        }
+    }
+
+    private getPhotoUrls(attachments: any[]) {
+        return attachments.filter((a : any) => a.type === "photo").map((a : any) => a.largePreviewUrl);
+    }
+
+    private logError(error: any) {
+        console.log(`[ERROR] ${error}`);
+    }
+
+    private sendDiscordBotMessage(message: any) {
+        // @ts-ignore
+        this.api.getThreadInfo(message.threadID, 
+            this.matchCallback(threadInfo => {
+                const senderName = threadInfo.nicknames[message.senderID];
+                // @ts-ignore
+                this.api.getUserInfo(message.senderID, 
+                    this.matchCallback(userInfo => {
+                        const images = this.getPhotoUrls(message.attachments);
+                        const userProfileThumbnail = userInfo[message.senderID].thumbSrc;
+                        this.bot?.sendMeskaSrodaMessage(senderName, message.body, images, userProfileThumbnail);
                     })
-                }
-                }
-            });
-        });
-
-        
+                );
+            })
+        );
     }
 
     sendMessage(text: string) {
@@ -75,21 +108,3 @@ class Mess {
 }
 
 export default Mess;
-
-// mutex.runExclusive(() => {
-//     ++messagesCounter;
-
-//     if (messagesCounter == repeatEveryMessagesCountThreshold) {
-//       messagesCounter = 0;
-
-//       api.sendMessage(
-//         repeaterMessage,
-//         message.threadID,
-//         (err, messageInfo) => {
-//           if (err) {
-//             return console.error(err);
-//           }
-//         }
-//       );
-//     }
-//   });
