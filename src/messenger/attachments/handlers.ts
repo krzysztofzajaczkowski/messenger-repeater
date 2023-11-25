459849b3301fdf678bf5365c4214b3213f25a0e8
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2023-11-25 15:02:53 +0100

    Proxy attachments from Messenger to Discord

---
+++ src/messenger/attachments/handlers.ts
@@ -0,0 +1,11 @@
+import { AttachmentHandler } from "./attachmentHandler";
+
+export const attachmentHandlers = [
+    new AttachmentHandler('sticker', att => att.url),
+    new AttachmentHandler('file', att => att.url),
+    new AttachmentHandler('photo', att => att.largePreviewUrl),
+    new AttachmentHandler('animated_image', att => att.url),
+    new AttachmentHandler('video', att => att.url),
+    new AttachmentHandler('audio', att => att.url),
+    new AttachmentHandler('share', att => att.url),
+]
